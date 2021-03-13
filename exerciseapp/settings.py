63402d07b1b92cd70728bcde08ca7f496dd5e5ff
John Chrosniak <chrosniakj@gmail.com>
2021-03-12 21:46:51 -0500

    updated settings.py

--- exerciseapp/settings.py
+++ exerciseapp/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'hoosfit.apps.HoosfitConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -39,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
-    'hoosfit',
+    
 
     'allauth',
     'allauth.account',
