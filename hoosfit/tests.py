9cc22b4d80c150ac0a0891c1b0f23e606c23bd56
John Chrosniak <chrosniakj@gmail.com>
2021-05-06 10:57:39 -0400

    added tests for exercise model

--- hoosfit/tests.py
+++ hoosfit/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Exercise, Award, Workout
 
-# Create your tests here.
+class ExerciseModelTest(TestCase):
+    def test_duplicate_exercise(self):
+        user = User.objects.create_user(username='test', email='test@virginia.edu', password='password')
+        exercise = Exercise.objects.create(user=user, exercise_name='test')
+        duplicate_exercise = Exercise.objects.create(user=user, exercise_name='test')
+        qs = Exercise.objects.filter(user__exact =user, date=datetime.date(2000,1,1))
+        self.assertEqual(qs.count() == 1)
